f7ab00b76382d27457793f23ce06c3bfb35e601e
MufeezTss <99019838+MufeezTss@users.noreply.github.com>
2022-02-21 11:26:08 +0530

    updated 21/02

--- Js/login.js
+++ Js/login.js
@@ -3,11 +3,11 @@ function myFunc() {
 
     var sub = {
       name:document.getElementById('name').value,
-      // email:document.getElementById('email').value,
       pass:document.getElementById('pass').value
     }
 
     localStorage.setItem('sub', JSON.stringify(sub));
-    alert(localStorage.getItem('sub'));
+    // localStorage.getItem('sub', JSON.stringify(sub));
+    
 
 }
